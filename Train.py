2e4acd333a4452efc050d1cca262d0b07461b0ac
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-11 15:03:08 +1100

    try gpu

--- Train.py
+++ Train.py
@@ -24,7 +24,8 @@ import json
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   # Restrict TensorFlow to only use the first GPU
