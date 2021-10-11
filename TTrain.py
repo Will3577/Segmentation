910ae6f94f11869a79a3b6eef028fa2c65181354
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-11 18:01:05 +1100

    pytorch implementation

--- TTrain.py
+++ TTrain.py
@@ -110,10 +110,14 @@ for x in (ids_test_x):
     base=os.path.basename(x)
     fn = os.path.splitext(base)[0]
     y = glob.glob(config['TEST_PATH_GT']+fn+'*')[0]
-    x_img = img_to_array(load_img(x, color_mode='rgb', target_size=[im_width,im_height]))
+    # x_img = img_to_array(load_img(x, color_mode='rgb', target_size=[im_width,im_height]))
+    x_img = cv2.imread(x)
+    x_img = cv2.resize(x_img,(1000,1000))
     x_img = x_img/255.0
     # Load masks
-    mask = img_to_array(load_img(y, color_mode='grayscale', target_size=[im_width,im_height]))
+    # mask = img_to_array(load_img(y, color_mode='grayscale', target_size=[im_width,im_height]))
+    mask = cv2.imread(y,0)
+    mask = cv2.resize(mask,(1000,1000))
     mask = mask/255.0
     #X_test[count] = x_img/255.0
     #y_test[count] = mask/255.0
@@ -135,6 +139,7 @@ y_test = np.array(y_test)
 
 input_img = Input((256, 256, 3), name='img')
 #from tensorflow.keras.utils.vis_utils import plot_model
+import segmentation_models_pytorch as smp
 
 if config['Model'] == "UNETMOD":
     print("Loading UNETMOD Model")
