fb0a1bd2ed1dad8a3718fcee058ca5f0cd660945
Hengrui  Wang <u6202156@anu.edu.au>
2021-10-11 22:44:48 +1100

    pytorch implementation

--- TTT.py
+++ TTT.py
@@ -54,7 +54,7 @@ net = smp.Unet(
         encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
         encoder_weights="imagenet",     # use `imagenet` pre-trained weights for encoder initialization
         in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-        classes=1,                      # model output channels (number of classes in your dataset)
+        classes=2,                      # model output channels (number of classes in your dataset)
     )
 
 from tqdm import tqdm
